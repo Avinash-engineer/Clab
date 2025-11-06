c58dadb4dca91c2e1cd63584fade725e39ad8e65
Avinash-engineer <avinashramoju393@gmail.com>
2025-11-06 15:19:15 +0530

    Add files via upload

---
+++ sqrt of number.c
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+#include<math.h>  
+int main() {
+double a,result;
+printf("Enter a value:");
+scanf("%lf",&a);
+result=sqrt(a);
+printf("square root of %.2lf=%.2lf",a,result);
+return 0;
+} 
+
